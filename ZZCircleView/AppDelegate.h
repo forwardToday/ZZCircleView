e7ad7b573264601096a3786ef4a1a477683823ea
zz <forwardtoday@aliyun.com>
2016-04-15 15:39:14 +0800

    Initial Commit

---
+++ ZZCircleView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZZCircleView
+//
+//  Created by jerry on 16/4/15.
+//  Copyright © 2016年 bobokeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZZCircleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZZCircleView
+//
+//  Created by jerry on 16/4/15.
+//  Copyright © 2016年 bobokeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
